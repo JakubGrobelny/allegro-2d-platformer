009f6c81a21569c2cae90ca513c3a0f5c432d197
JakubGrobelny <300481@uwr.edu.pl>
2018-01-21 18:28:24 +0100

    System kolizji DZIAŁA

--- src/object_updates.c
+++ src/object_updates.c
@@ -66,16 +66,62 @@ void apply_vectors(Object* object, ObjectsList* list)
     else if (object->physics.speed.y < 0)
         dir_y = TOP;
 
+    if (dir_y == dir_x) // static
+        return;
+
+
+    if (dir_y != BOTTOM)
+        new_x.pos_y--;
+    if (dir_x == LEFT)
+        new_y.pos_x++;
+    if (dir_x == RIGHT)
+        new_y.pos_x--;
+
     for (int i = 0; i < list->size; i++)
     {
         if (relative_direction(object, get_element_pointer_ol(list, i), dir_x) || relative_direction(object, get_element_pointer_ol(list, i), dir_y))
         {
-            if (collide(new_x, get_element_pointer_ol(list, i)->hitbox))
-                object->physics.speed.x = 0;
-            if (collide(new_y, get_element_pointer_ol(list, i)->hitbox))
-                object->physics.speed.y = 0;
+            Hitbox obstacle = get_element_pointer_ol(list, i)->hitbox;
+
+            if (collide(new_x, obstacle))
+            {
+                // adjust speed
+                int adjustment = 0;
+
+                if (dir_x == LEFT)
+                {
+                    adjustment = (obstacle.pos_x + obstacle.width) - new_x.pos_x;
+                }
+                else if (dir_x == RIGHT)
+                {
+                    adjustment = obstacle.pos_x - (new_x.pos_x + new_x.width);
+                    printf("adjustment_x_RIGHT = %d - (%d + %d) = %d, speed = %f\n", obstacle.pos_x, new_x.pos_x, new_x.width, adjustment, object->physics.speed.x);
+                }
+
+                object->physics.speed.x += adjustment;
+            }
+            if (collide(new_y, obstacle))
+            {
+                // adjust speed
+                int adjustment = 0;
+
+                if (dir_y == TOP)
+                {
+                    printf("adjustment_y_TOP = (%d + %d) - %d = %d, speed = %f\n", obstacle.pos_y, obstacle.height, new_y.pos_y, adjustment, object->physics.speed.y);
+                    adjustment = (obstacle.pos_y + obstacle.height) - new_y.pos_y;
+                }
+                else if (dir_y == BOTTOM)
+                {
+                    printf("adjustment_y_BOTTOM = %d - (%d + %d) = %d, speed = %f\n", obstacle.pos_y, new_y.pos_y, new_y.width, adjustment, object->physics.speed.y);
+                    adjustment = obstacle.pos_y - (new_y.pos_y + new_y.height);
+                }
+
+                object->physics.speed.y += adjustment;
+            }
         }
     }
+
+    printf("speed.x = %f, speed.y = %f\n", object->physics.speed.x, object->physics.speed.y);
 
     object->pos_x += (int)object->physics.speed.x;
     object->pos_y += (int)object->physics.speed.y;
